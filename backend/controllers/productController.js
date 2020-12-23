c7b3e1ad0fcd4aa5807b7b2dec64634e570e81ac
Vladimir <ecofovka@gmail.com>
2020-12-23 19:12:05 +0100

    Authorization backend

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// Fetrch all products: GET /api/products (public)
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// Fetrch a single product: GET /api/products/:id (public)
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById };
