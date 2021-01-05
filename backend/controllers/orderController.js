d110dba63fd8838b6cf2e536ac452aca2ce02ab2
Vladimir <ecofovka@gmail.com>
2021-01-05 19:16:47 +0100

    backend & frontend: add putting order to database & orderScreen

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,48 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+
+// Create new order: POST /api/orders (private)
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+      user: req.user._id,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
+
+// Get order by ID: GET /api/orders/:id (private)
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
